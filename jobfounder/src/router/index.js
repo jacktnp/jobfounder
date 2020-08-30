383dab781513def705a34b36176174041c9629dd
Thanapon Wongprasert <thanaponpkw@gmail.com>
2020-08-30 14:55:05 +0700

    v0.1

--- jobfounder/src/router/index.js
+++ jobfounder/src/router/index.js
@@ -5,11 +5,26 @@ import authen from '@/pages/authen'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'authen',
       component: authen
-    }
+    },
+    { path: '*', redirect: '/' }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
 })
